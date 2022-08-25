1562ca3378546b9d9f7bfb92baa1039529830063
TiggerTiff <tiffanybell031@gmail.com>
2022-08-25 10:16:41 +1000

    add prevent default event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+ event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -9,6 +11,6 @@ var createTaskHandler = function() {
  
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
